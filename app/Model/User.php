03cd6658da6e990eca8d3d0598f339f4ab071eb6
Ilya <ilya_dreytser@yahoo.com>
2014-08-07 19:28:05 -0400

    fixing 'add' to UsersController beforeFilter

--- app/Model/User.php
+++ app/Model/User.php
@@ -35,7 +35,7 @@ class User extends AppModel {
 	
 	 public function beforeSave($options = array()) {
         if (isset($this->data[$this->alias]['password'])) {
-            $passwordHasher = new SimplePasswordHasher();
+            $passwordHasher = new BlowfishPasswordHasher();
             $this->data[$this->alias]['password'] = $passwordHasher->hash(
                 $this->data[$this->alias]['password']
             );
